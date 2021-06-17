02b589b5fb165533fde6cb264b9de36560144a44
rushab17 <rushabshah120@gmail.com>
2021-06-17 21:55:57 +0530

    Resolved merge conflicts

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 const Color statusBarColor = Color(0xFFFFFFFF);
-
 const Color darkBlueColor = Color(0xFF152968);
 const Color grey = Color(0xFF828282);
 const Color backgroundColor = Color(0xFFF5F5F5);
+const Color whiteBackgroundColor = Color(0xFFF9F9F9);
 const Color lightBlue = Color(0xFF1A73E9);
 const Color black = Colors.black;
 const Color unselectedGrey = Color(0xFF9B9B9B);
@@ -23,10 +23,9 @@ const Color flagOrange = Color(0xffFF9933);
 const Color flagGreen = Color(0xff39B82D);
 const Color backgroundGrey = Color(0xffECEEF1);
 const Color black_87 = Colors.black87;
-const Color whiteBackgroundColor = Color(0xFFF9F9F9);
 Color loadingWidgetColor = Colors.lightBlueAccent;
-Color loadScreenPrimaryTextColor = Color(0xFF555454);
-Color loadScreenSecondaryTextColor = Color(0xFF2A2E3B);
+//TODO: Remove duplicate color entries , choose a general name for solidLineColor ,priceBackground , unactivereset and replace it in theor usecase too
+// keep deactiveButton color though othervise code will break
 Color solidLineColor = Color(0xFFC4C9D9);
 Color priceBackground = Color(0xFFC4C9D9);
 Color bidBackground = Color(0xFF152968);
@@ -50,3 +49,6 @@ Color blueTitleColor = Color(0xFF152968);
 Color borderLightColor = Color(0xFF878787);
 Color textLightColor = Color(0xFF979797);
 Color liveasyOrange = Color(0xFFFF7600);
+Color unactiveReset = Color(0xffC4C9D9);
+Color loadScreenPrimaryTextColor = Color(0xFF555454);
+Color loadScreenSecondaryTextColor = Color(0xFF2A2E3B);
