75eb34a7209d94b0e5d3755f1926ca569d06bd0c
uttkarshrastogi <ruttkarsh.uttkarsh@gmail.com>
2021-06-24 22:21:39 +0530

    Merge remote-tracking branch 'origin/conflict' into liveasy97-dev2
    # Conflicts:
    #	lib/constants/color.dart
    #	lib/models/cardsModal.dart
    #	lib/models/loadApiModel.dart
    #	lib/providerClass/providerData.dart
    #	lib/screens/cityNameInputScreen.dart
    #	lib/screens/findLoadScreen.dart
    #	lib/screens/navigationScreen.dart
    #	lib/widgets/addTruckCircularButtonTemplate.dart
    #	lib/widgets/linePainter.dart
    #	lib/widgets/suggestedLoadsWidget.dart
    #	pubspec.yaml

--- lib/screens/postLoadScreen.dart
+++ lib/screens/postLoadScreen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:liveasy/constants/color.dart';
 import 'package:liveasy/constants/spaces.dart';
 import 'package:liveasy/providerClass/providerData.dart';
+import 'package:liveasy/screens/deliveredScreen.dart';
+import 'package:liveasy/screens/onGoingScreen.dart';
+import 'package:liveasy/screens/orderScreens/order.dart';
 import 'package:liveasy/widgets/Header.dart';
 import 'package:liveasy/widgets/OrderScreenNavigationBarButton.dart';
 import 'package:provider/provider.dart';
@@ -14,7 +17,10 @@ class PostOrdersScreen extends StatefulWidget {
 }
 
 class _PostOrdersScreenState extends State<PostOrdersScreen> {
-  List screens = [Text('loads'), Text('on going'), Text('delivered')];
+  //Page Controller
+  PageController pageController = PageController(initialPage: 0);
+
+  int currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -33,9 +39,18 @@ class _PostOrdersScreenState extends State<PostOrdersScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  OrderScreenNavigationBarButton(text: 'My Loads', value: 0),
-                  OrderScreenNavigationBarButton(text: 'On-going', value: 1),
-                  OrderScreenNavigationBarButton(text: 'Delivered', value: 2)
+                  OrderScreenNavigationBarButton(
+                      text: 'My Loads',
+                      value: 0,
+                      pageController: pageController),
+                  OrderScreenNavigationBarButton(
+                      text: 'On-going',
+                      value: 1,
+                      pageController: pageController),
+                  OrderScreenNavigationBarButton(
+                      text: 'Delivered',
+                      value: 2,
+                      pageController: pageController)
                 ],
               ),
               Divider(
@@ -43,8 +58,21 @@ class _PostOrdersScreenState extends State<PostOrdersScreen> {
                 thickness: 1,
               ),
               Container(
-                child: screens[providerData.upperNavigatorIndex],
-              ),
+                height: 600,
+                child: PageView(
+                  controller: pageController,
+                  onPageChanged: (value) {
+                    setState(() {
+                      providerData.updateUpperNavigatorIndex(value);
+                    });
+                  },
+                  children: [
+                    order(),
+                    OngoingScreen(),
+                    DeliveredScreen(),
+                  ],
+                ),
+              )
             ],
           ),
         ),
