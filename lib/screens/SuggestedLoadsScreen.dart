1f4816952d1729dcee70ba734c7168098cd62cfa
Atharv Joshi <joshiatharv1078@protonmail.com>
2021-07-24 19:30:38 +0530

    Revert "smooth scrooling in suggested loads screen"

--- lib/screens/SuggestedLoadsScreen.dart
+++ lib/screens/SuggestedLoadsScreen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:liveasy/constants/color.dart';
 import 'package:liveasy/constants/spaces.dart';
-import 'package:liveasy/models/loadApiModel.dart';
+import 'package:liveasy/functions/getLoadPosterDetailsFromApi.dart';
 import 'package:liveasy/models/loadDetailsScreenModel.dart';
+import 'package:liveasy/models/loadPosterModel.dart';
 import 'package:liveasy/widgets/Header.dart';
 import 'package:liveasy/widgets/buttons/filterButton.dart';
 import 'package:liveasy/widgets/loadingWidget.dart';
@@ -39,26 +40,54 @@ class _SuggestedLoadScreenState extends State<SuggestedLoadScreen> {
     jsonData = await jsonDecode(response.body);
 
     for (var json in jsonData) {
-      LoadDetailsScreenModel model = LoadDetailsScreenModel();
-      model.loadId = json["loadId"] != null ? json['loadId'] : 'NA';
-      model.loadingPoint = json["loadingPoint"] != null ? json['loadingPoint'] : 'NA';
-      model.loadingPointCity = json["loadingPointCity"] != null ? json['loadingPointCity'] : 'NA';
-      model.loadingPointState = json["loadingPointState"] != null ? json['loadingPointState'] : 'NA';
-      model.postLoadId = json["postLoadId"] != null ? json['postLoadId'] : 'NA';
-      model.unloadingPoint = json["unloadingPoint"] != null ? json['unloadingPoint'] : 'NA';
-      model.unloadingPointCity = json["unloadingPointCity"] != null ? json['unloadingPointCity'] : 'NA';
-      model.unloadingPointState = json["unloadingPointState"] != null ? json['unloadingPointState'] : 'NA';
-      model.productType = json["productType"] != null ? json['productType'] : 'NA';
-      model.truckType = json["truckType"] != null ? json['truckType'] : 'NA';
-      model.noOfTrucks = json["noOfTrucks"] != null ? json['noOfTrucks'] : 'NA';
-      model.weight = json["weight"] != null ? json['weight'] : 'NA';
-      model.comment = json["comment"] != null ? json['comment'] : 'NA';
-      model.status = json["status"] != null ? json['status'] : 'NA';
-      model.loadDate = json["loadDate"] != null ? json['loadDate'] : 'NA';
-      model.rate = json["rate"] != null ? json['rate'].toString() : 'NA';
-      model.unitValue = json["unitValue"] != null ? json['unitValue'] : 'NA';
+      LoadDetailsScreenModel loadDetailsScreenModel = LoadDetailsScreenModel();
+      loadDetailsScreenModel.loadId = json["loadId"] != null ? json['loadId'] : 'NA';
+      loadDetailsScreenModel.loadingPoint = json["loadingPoint"] != null ? json['loadingPoint'] : 'NA';
+      loadDetailsScreenModel.loadingPointCity = json["loadingPointCity"] != null ? json['loadingPointCity'] : 'NA';
+      loadDetailsScreenModel.loadingPointState = json["loadingPointState"] != null ? json['loadingPointState'] : 'NA';
+      loadDetailsScreenModel.postLoadId = json["postLoadId"] != null ? json['postLoadId'] : 'NA';
+      loadDetailsScreenModel.unloadingPoint = json["unloadingPoint"] != null ? json['unloadingPoint'] : 'NA';
+      loadDetailsScreenModel.unloadingPointCity = json["unloadingPointCity"] != null ? json['unloadingPointCity'] : 'NA';
+      loadDetailsScreenModel.unloadingPointState = json["unloadingPointState"] != null ? json['unloadingPointState'] : 'NA';
+      loadDetailsScreenModel.productType = json["productType"] != null ? json['productType'] : 'NA';
+      loadDetailsScreenModel.truckType = json["truckType"] != null ? json['truckType'] : 'NA';
+      loadDetailsScreenModel.noOfTrucks = json["noOfTrucks"] != null ? json['noOfTrucks'] : 'NA';
+      loadDetailsScreenModel.weight = json["weight"] != null ? json['weight'] : 'NA';
+      loadDetailsScreenModel.comment = json["comment"] != null ? json['comment'] : 'NA';
+      loadDetailsScreenModel.status = json["status"] != null ? json['status'] : 'NA';
+      loadDetailsScreenModel.loadDate = json["loadDate"] != null ? json['loadDate'] : 'NA';
+      loadDetailsScreenModel.rate = json["rate"] != null ? json['rate'].toString() : 'NA';
+      loadDetailsScreenModel.unitValue = json["unitValue"] != null ? json['unitValue'] : 'NA';
+
+
+      LoadPosterModel loadPosterModel =  await getLoadPosterDetailsFromApi(loadPosterId: loadDetailsScreenModel.postLoadId.toString());
+
+
+      if(loadPosterModel != null){
+        loadDetailsScreenModel.loadPosterId =  loadPosterModel.loadPosterId  ;
+        loadDetailsScreenModel.phoneNo = loadPosterModel.loadPosterPhoneNo  ;
+        loadDetailsScreenModel.loadPosterLocation = loadPosterModel.loadPosterLocation  ;
+        loadDetailsScreenModel.loadPosterName = loadPosterModel.loadPosterName  ;
+        loadDetailsScreenModel.loadPosterCompanyName = loadPosterModel.loadPosterCompanyName  ;
+        loadDetailsScreenModel.loadPosterKyc = loadPosterModel.loadPosterKyc  ;
+        loadDetailsScreenModel.loadPosterCompanyApproved = loadPosterModel.loadPosterCompanyApproved ;
+        loadDetailsScreenModel.loadPosterApproved = loadPosterModel.loadPosterApproved  ;
+      }
+      else{
+        //this will run when postloadId value is something different than uuid , like random text entered from postman
+        loadDetailsScreenModel.loadPosterId =  'NA';
+        loadDetailsScreenModel.phoneNo = '';
+        loadDetailsScreenModel.loadPosterLocation = 'NA'  ;
+        loadDetailsScreenModel.loadPosterName = 'NA'  ;
+        loadDetailsScreenModel.loadPosterCompanyName = 'NA'  ;
+        loadDetailsScreenModel.loadPosterKyc = 'NA'  ;
+        loadDetailsScreenModel.loadPosterCompanyApproved = true ;
+        loadDetailsScreenModel.loadPosterApproved = true  ;
+      }
+
+
       setState(() {
-        data.add(model);
+        data.add(loadDetailsScreenModel);
       });
     }
     setState(() {
