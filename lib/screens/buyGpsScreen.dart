7986a9af63c1aa45172c37671bc6da63e0d7f5a4
Jay Firke <firke0419@gmail.com>
2021-08-14 18:07:39 +0530

    Added Buy GPS Screen

--- lib/screens/buyGpsScreen.dart
+++ lib/screens/buyGpsScreen.dart
@@ -1,4 +1,3 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
@@ -10,161 +9,19 @@ import 'package:liveasy/constants/radius.dart';
 import 'package:liveasy/constants/spaces.dart';
 import 'package:liveasy/controller/transporterIdController.dart';
 import 'package:liveasy/functions/buyGPSApiCalls.dart';
+import 'package:liveasy/functions/truckApis/getTruckDataWithPageNo.dart';
 import 'package:liveasy/models/truckModel.dart';
 import 'package:liveasy/widgets/Header.dart';
 import 'package:flutter_config/flutter_config.dart';
-import 'package:http/http.dart' as http;
+import 'package:liveasy/widgets/alertDialog/CompletedDialog.dart';
 import 'package:liveasy/widgets/alertDialog/buyGPSAddTruckDialog.dart';
 import 'package:liveasy/widgets/alertDialog/nextUpdateAlertDialog.dart';
 import 'package:liveasy/widgets/alertDialog/sameTruckAlertDialogBox.dart';
+import 'package:liveasy/widgets/buttons/buyGPSRadioButtons.dart';
 import 'package:liveasy/widgets/buttons/helpButton.dart';
 import 'package:liveasy/widgets/loadingWidgets/truckLoadingLongWidgets.dart';
 import 'package:liveasy/widgets/searchLoadWidget.dart';
 import 'package:liveasy/widgets/trucksLongCard.dart';
-
-class MyRadioOption<T> extends StatelessWidget {
-
-  final T value;
-  final T duration;
-  final T? groupValue;
-  final T? groupDurationValue;
-  final String text;
-  final ValueChanged<T?> onChanged;
-  final ValueChanged<T?> onDurationChanged;
-
-  const MyRadioOption({
-    required this.groupDurationValue,
-    required this.duration,
-    required this.value,
-    required this.groupValue,
-    required this.text,
-    required this.onChanged,
-    required this.onDurationChanged
-  });
-
-  Widget _buildLabel() {
-    final bool isSelected = value == groupValue;
-    return Container(
-      padding: EdgeInsets.only(left: space_2),
-      margin: EdgeInsets.fromLTRB(0, (space_3 - 1), 0, 0),
-      width: (space_40 + space_18),
-      height: (space_18 + 2),
-      decoration: BoxDecoration(
-          color: isSelected ? bidBackground : white,
-          borderRadius: BorderRadius.circular((radius_1 + 2))
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image(
-            image: isSelected ? AssetImage(
-                'assets/icons/selectedIcon.png'
-            ) : AssetImage(
-                'assets/icons/blueCircle.png'
-            ),
-            height: space_4,
-            width: space_4,
-          ),
-          SizedBox(
-            width: space_4,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildText(),
-              SizedBox(
-                height: space_3,
-              ),
-              Row(
-                children: [
-                  Container(
-                    child: Image(
-                      image: AssetImage(
-                          'assets/icons/tickIcon.png'),
-                      height: (space_2 - 1),
-                      width: (space_2 - 3),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  Container(
-                    child: Text(
-                      "Truck location",
-                      style: TextStyle(
-                          color: isSelected ? white : bidBackground,
-                          fontWeight: regularWeight,
-                          fontSize: size_6
-                      ),
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: (space_1 - 2),
-              ),
-              Row(
-                children: [
-                  Container(
-                    child: Image(
-                      image: AssetImage(
-                          'assets/icons/tickIcon.png'
-                      ),
-                      height: (space_2 - 1),
-                      width: (space_2 - 3),
-                    ),
-                  ),
-                  SizedBox(
-                    width: space_1,
-                  ),
-                  Container(
-                    child: Text(
-                      "1 year replacement warranty",
-                      style: TextStyle(
-                          color: isSelected ? white : bidBackground,
-                          fontWeight: regularWeight,
-                          fontSize: size_6
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget _buildText() {
-    final bool isSelected = value == groupValue;
-    return Text(
-      text,
-      style: TextStyle(
-          color: isSelected ? white : bidBackground,
-          fontWeight: mediumBoldWeight,
-          fontSize: size_7
-      ),
-    );
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => {
-        onChanged(value),
-        onDurationChanged(duration),
-      },
-        child: Row(
-          children: [
-            _buildLabel()
-          ],
-        ),
-    );
-  }
-}
 
 class BuyGpsScreen extends StatefulWidget {
   const BuyGpsScreen({Key? key}) : super(key: key);
@@ -181,8 +38,7 @@ class _BuyGpsScreenState extends State<BuyGpsScreen> {
   TruckModel truckModel = TruckModel();
   ScrollController scrollController = ScrollController();
   String? truckID;
-  List truckNUMBERDataList = [];
-  List truckIDDataList = [];
+  var truckDataList = [];
   bool loading = false;
   late List jsonData;
   int i = 0;
@@ -330,7 +186,7 @@ class _BuyGpsScreenState extends State<BuyGpsScreen> {
                             onTap: () => {
                               showDialog(
                                   context: context,
-                                  builder: (context) => buyGPSAddTruckDialog()),
+                                  builder: (context) => BuyGPSAddTruckDialog()),
                             },
                               child: Row(
                                 children: [
@@ -364,7 +220,7 @@ class _BuyGpsScreenState extends State<BuyGpsScreen> {
                             width: double.infinity,
                             child: loading
                                 ? TruckLoadingLongWidgets()
-                                : truckNUMBERDataList.isEmpty
+                                : truckDataList.isEmpty
                                 ? Container(
                               // height: MediaQuery.of(context).size.height * 0.27,
                               child: Container(
@@ -392,13 +248,13 @@ class _BuyGpsScreenState extends State<BuyGpsScreen> {
                                 : ListView.builder(
                                 controller: scrollController,
                                 padding: EdgeInsets.only(bottom: space_15),
-                                itemCount: truckNUMBERDataList.length,
+                                itemCount: truckDataList.length,
                                 scrollDirection: Axis.vertical,
                                 itemBuilder: (context, index) {
                                   return GestureDetector(
                                     onTap: () {
                                       _onSelected(index);
-                                      truckID = truckIDDataList[index];
+                                      truckID = truckDataList[index].truckId;
                                       if(isRadioButtonDisable == false || truckID == null) {
                                         isButtonDisable = false;
                                       } else {
@@ -406,10 +262,10 @@ class _BuyGpsScreenState extends State<BuyGpsScreen> {
                                       }
                                     },
                                     child: TrucksLongCard(
+                                      truckData: truckDataList[index],
                                       borderCard: _selectedIndex != null && _selectedIndex == index
                                           ? Border.all(color: bidBackground)
-                                          : null,
-                                      TruckNo: "${truckNUMBERDataList[index]}",
+                                          : null
                                     ),
                                   );
                                 }),
@@ -455,7 +311,12 @@ class _BuyGpsScreenState extends State<BuyGpsScreen> {
                                           truckId: truckID);
                                       if (gpsId != null) {
                                         EasyLoading.dismiss();
-                                        Get.snackbar("Successfully Purchased", "$gpsId");
+                                        showDialog(
+                                            context: context,
+                                            builder: (context) => completedDialog(
+                                              upperDialogText: "You’ve purchased GPS successfully!",
+                                              lowerDialogText: "",
+                                            ));
                                       } else {
                                         EasyLoading.dismiss();
                                         showDialog(
@@ -501,35 +362,13 @@ class _BuyGpsScreenState extends State<BuyGpsScreen> {
   }
 
   getTruckData(int i) async {
-    http.Response response = await http.get(Uri.parse(
-        '$truckApiUrl?transporterId=${transporterIdController.transporterId.value}&pageNo=$i'));
-    jsonData = json.decode(response.body);
-    for (var json in jsonData) {
-      truckModel.truckId = json["truckId"] != null ? json["truckId"] : 'NA';
-      truckModel.transporterId =
-      json["transporterId"] != null ? json["transporterId"] : 'NA';
-      truckModel.truckNo = json["truckNo"] != null ? json["truckNo"] : 'NA';
-      truckModel.truckApproved =
-      json["truckApproved"] != null ? json["truckApproved"] : false;
-      truckModel.imei = json["imei"] != null ? json["imei"] : 'NA';
-      truckModel.passingWeightString = json["passingWeight"] != null
-          ? json["passingWeight"].toString()
-          : 'NA';
-      truckModel.truckType =
-      json["truckType"] != null ? json["truckType"] : 'NA';
-      truckModel.driverId = json["driverId"] != null ? json["driverId"] : 'NA';
-      truckModel.tyresString =
-      json["tyres"] != null ? json["tyres"].toString() : 'NA';
-      truckModel.truckLengthString =
-      json["truckLength"] != null ? json["truckLength"].toString() : 'NA';
-      setState(() {
-        truckNUMBERDataList.add(truckModel.truckNo);
-        truckIDDataList.add(truckModel.truckId);
-      });
-    } //for loop
+    var truckDataListForPagei = await getTruckDataWithPageNo(i);
+    print("${truckDataListForPagei[0].truckNo}");
+    for (var truckData in truckDataListForPagei){
+      truckDataList.add(truckData);}
     setState(() {
       loading = false;
     });
-  } //getTruckData
+  }//getTruckData
 
 }
