1bf6bd9e747122b5dd9b26f9b697ecb4a84d2d38
chirag4601 <chirag4601@gmail.com>
2021-09-07 16:44:07 +0530

    resolved error in postLoadScreen weight selection

--- lib/widgets/alertDialog/addDriverAlertDialog.dart
+++ lib/widgets/alertDialog/addDriverAlertDialog.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -251,7 +253,6 @@ class _AddDriverAlertDialogState extends State<AddDriverAlertDialog> {
                     );
                   }
                 }
-              },
             ),
             CancelButtonForAddNewDriver()
           ],
