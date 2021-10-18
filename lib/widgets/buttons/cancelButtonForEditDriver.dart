850d6931a9a033f666b253cb3c7f66b87e75e823
bibek ranjan saha <58353328+bibek-ranjan-saha@users.noreply.github.com>
2021-10-18 15:05:44 +0530

    added edit driver

---
+++ lib/widgets/buttons/cancelButtonForEditDriver.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:liveasy/constants/color.dart';
+import 'package:liveasy/constants/fontSize.dart';
+import 'package:liveasy/constants/fontWeights.dart';
+import 'package:liveasy/constants/spaces.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+
+class CancelButtonForEditDriver extends StatefulWidget {
+
+  @override
+  _CancelButtonForEditDriverState createState() => _CancelButtonForEditDriverState();
+}
+
+class _CancelButtonForEditDriverState extends State<CancelButtonForEditDriver> {
+  @override
+  Widget build(BuildContext context) {
+
+    return GestureDetector(
+      onTap: () =>Get.back(),
+      child: Container(
+        margin: EdgeInsets.only(right: space_3),
+        height: space_6 + 1,
+        width: space_16,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(color: darkBlueColor)),
+        child: Center(
+          child: Text(
+            AppLocalizations.of(context)!.cancel,
+            style: TextStyle(
+                color: Colors.black,
+                fontWeight: normalWeight,
+                fontSize: size_6 + 2),
+          ),
+        ),
+      ),
+    );
+  }
+}
