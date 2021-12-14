120c3a4ec98053659b293d24ed7d7fba1e661804
Kripa <kripajethwani@gmail.com>
2021-12-14 16:57:27 +0530

    change

--- lib/screens/trackScreen.dart
+++ lib/screens/trackScreen.dart
@@ -15,7 +15,6 @@ import 'package:liveasy/constants/fontWeights.dart';
 import 'package:liveasy/constants/spaces.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
-import 'package:liveasy/screens/animarker.dart';
 import 'package:liveasy/screens/playRouteHistoryScreen.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:liveasy/functions/trackScreenFunctions.dart';
