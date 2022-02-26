3487cc898dc5f05e48a6707c3757e0707ae2ee73
Akshay <39567885+edaxe@users.noreply.github.com>
2022-02-26 17:40:42 +0530

    Completed Nearby Places Locator, localization pending

--- lib/screens/nearbyPlacesScreen.dart
+++ lib/screens/nearbyPlacesScreen.dart
@@ -5,7 +5,7 @@ import 'dart:typed_data';
 import 'package:async/async.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
-import 'package:geocoding/geocoding.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter/material.dart';
 import 'package:liveasy/constants/color.dart';
 import 'package:liveasy/constants/spaces.dart';
@@ -18,8 +18,6 @@ import 'package:liveasy/models/placesNearbyDataModel.dart';
 import 'package:liveasy/widgets/Header.dart';
 import 'package:liveasy/widgets/buttons/helpButton.dart';
 import 'package:liveasy/widgets/nearbyPlaceInfoCard.dart';
-import 'package:liveasy/widgets/nearbyPlaceScreenDetailsWidget.dart';
-import 'package:liveasy/widgets/nearbyPlacesDetailsCard.dart';
 import 'package:logger/logger.dart';
 import 'package:custom_info_window/custom_info_window.dart';
 import 'package:flutter_config/flutter_config.dart';
@@ -105,6 +103,7 @@ class _NearbyPlacesScreenState extends State<NearbyPlacesScreen>
             googleAPiKey);
 
     var response = await http.get(url);
+    if (response != null) EasyLoading.dismiss();
     var body = response.body;
     _placesNearbyData = PlacesNearbyData.fromJson(jsonDecode(body));
 
@@ -128,6 +127,19 @@ class _NearbyPlacesScreenState extends State<NearbyPlacesScreen>
   @override
   void initState() {
     super.initState();
+
+    EasyLoading.instance
+      ..indicatorType = EasyLoadingIndicatorType.ring
+      ..indicatorSize = 45.0
+      ..radius = 10.0
+      ..maskColor = darkBlueColor
+      ..userInteractions = false
+      ..backgroundColor = darkBlueColor
+      ..dismissOnTap = false;
+    EasyLoading.show(
+      status: "Loading...",
+    );
+
     WidgetsBinding.instance!.addObserver(this);
     from = yesterday.toIso8601String();
     to = now.toIso8601String();
@@ -536,11 +548,13 @@ class _NearbyPlacesScreenState extends State<NearbyPlacesScreen>
                           ),
                         ),
                       ),
-                      NearbyPlaceInfoCard(
-                          pinPillPosition: pinPillPosition,
-                          placesIndex: placesIndex,
-                          placeOnTheMapTag: widget.placeOnTheMapTag,
-                          placesNearbyData: _placesNearbyData),
+                      (_placesNearbyData.results != null)
+                          ? NearbyPlaceInfoCard(
+                              pinPillPosition: pinPillPosition,
+                              placesIndex: placesIndex,
+                              placeOnTheMapTag: widget.placeOnTheMapTag,
+                              placesNearbyData: _placesNearbyData)
+                          : Container(),
                     ])),
               ),
               Positioned(
@@ -576,18 +590,192 @@ class _NearbyPlacesScreenState extends State<NearbyPlacesScreen>
                 duration: Duration(milliseconds: 200),
                 left: 0,
                 bottom: (showBottomMenu) ? 0 : -(height / 3) + 36,
-                child: NearbyPlacesDetails(
-                  height: height,
-                  width: width,
-                  placeOnTheMapName: widget.placeOnTheMapName,
-                  placeOnTheMapTag: widget.placeOnTheMapTag,
-                  placesNearbyData: _placesNearbyData,
-                ),
+                child: nearbyPlacesList(height, width),
+                // child: NearbyPlacesDetails(
+                //   height: height,
+                //   width: width,
+                //   placeOnTheMapName: widget.placeOnTheMapName,
+                //   placeOnTheMapTag: widget.placeOnTheMapTag,
+                //   placesNearbyData: _placesNearbyData,
+                // ),
               )
             ],
           ),
         ),
       ),
     );
+  }
+
+  Container nearbyPlacesList(double height, double width) {
+    return Container(
+        height: height / 3 + 24,
+        width: width,
+        decoration: BoxDecoration(
+            color: white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+            boxShadow: [
+              BoxShadow(
+                color: darkShadow,
+                offset: const Offset(
+                  0,
+                  -5.0,
+                ),
+                blurRadius: 15.0,
+                spreadRadius: 10.0,
+              ),
+              BoxShadow(
+                color: white,
+                offset: const Offset(0, 1.0),
+                blurRadius: 0.0,
+                spreadRadius: 2.0,
+              ),
+            ]),
+        child:
+            ListView(physics: const NeverScrollableScrollPhysics(), children: <
+                Widget>[
+          Column(
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Divider(
+                  color: const Color(0xFFCBCBCB),
+                  // height: size_3,
+                  thickness: 3,
+                  indent: 150,
+                  endIndent: 150,
+                ),
+                Container(
+                    alignment: Alignment.center,
+                    margin:
+                        EdgeInsets.fromLTRB(space_1, space_1, space_1, space_1),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          width: 8,
+                        ),
+                        //Row for nearby location name and petrol prices
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Row(
+                              children: [
+                                Image.asset(
+                                  'assets/icons/' +
+                                      widget.placeOnTheMapTag +
+                                      '.png',
+                                  height: size_14,
+                                  width: size_14,
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Text(
+                                  widget.placeOnTheMapName,
+                                  style: TextStyle(
+                                      fontSize: size_10,
+                                      fontWeight: boldWeight,
+                                      color: darkBlueColor),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        //Listview of Petrol Pumps nearby with their location
+                        Container(
+                            height: height / 4 + 24,
+                            margin: EdgeInsets.fromLTRB(0, space_1, 0, 0),
+                            child: ListView.builder(
+                                shrinkWrap: true,
+                                physics: ClampingScrollPhysics(),
+                                scrollDirection: Axis.vertical,
+                                itemCount: (_placesNearbyData.results != null)
+                                    ? _placesNearbyData.results?.length
+                                    : 0,
+                                itemBuilder: (context, index) {
+                                  Results here =
+                                      _placesNearbyData.results![index];
+                                  print("DISTANCE ABCD " +
+                                      here.distance.toString());
+                                  return Card(
+                                      child: InkWell(
+                                    onTap: () {
+                                      this._googleMapController.animateCamera(
+                                              CameraUpdate.newCameraPosition(
+                                            CameraPosition(
+                                              bearing: 0,
+                                              target: LatLng(
+                                                  here.geometry!.location!.lat!,
+                                                  here.geometry!.location!
+                                                      .lng!),
+                                              zoom: this.zoom,
+                                            ),
+                                          ));
+                                    },
+                                    child: ListTile(
+                                        title: Text(
+                                          here.name.toString(),
+                                          style: TextStyle(
+                                              fontSize: size_7,
+                                              fontWeight: mediumBoldWeight,
+                                              color: darkBlueColor),
+                                        ),
+                                        subtitle: Text(
+                                          here.vicinity.toString(),
+                                          style: TextStyle(
+                                            fontSize: size_6,
+                                            fontWeight: mediumBoldWeight,
+                                          ),
+                                        ),
+                                        trailing: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceAround,
+                                          children: [
+                                            GestureDetector(
+                                              onTap: () {
+                                                openMap(here
+                                                        .geometry!.location!.lat
+                                                        .toString() +
+                                                    ',' +
+                                                    here.geometry!.location!.lng
+                                                        .toString());
+                                              },
+                                              child: Image.asset(
+                                                'assets/icons/navigateIcon.png',
+                                                height: size_14,
+                                                width: size_14,
+                                              ),
+                                            ),
+                                            Text(
+                                              here.distance!
+                                                      .toStringAsFixed(1) +
+                                                  " km",
+                                              style: TextStyle(
+                                                  fontSize: size_5,
+                                                  fontWeight: mediumBoldWeight,
+                                                  color: darkBlueColor),
+                                            ),
+                                          ],
+                                        )),
+                                  ));
+                                })),
+                      ],
+                    )),
+              ]),
+        ]));
+  }
+
+  Future<void> openMap(String coordinates) async {
+    String googleUrl =
+        'https://www.google.com/maps/search/?api=1&query=$coordinates';
+    if (await canLaunch(googleUrl)) {
+      await launch(googleUrl);
+    } else {
+      throw 'Could not open the map.';
+    }
   }
 }
