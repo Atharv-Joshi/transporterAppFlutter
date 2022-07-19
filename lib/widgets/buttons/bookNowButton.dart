e9a8d46f85fcf6a9b0e6e738e8679407f01dc539
nscspc <83803981+nscspc@users.noreply.github.com>
2022-07-19 20:13:26 +0530

    Update bookNowButton.dart

--- lib/widgets/buttons/bookNowButton.dart
+++ lib/widgets/buttons/bookNowButton.dart
@@ -10,6 +10,7 @@ import 'package:liveasy/functions/truckApis/truckApiCalls.dart';
 import 'package:liveasy/models/driverModel.dart';
 import 'package:liveasy/models/loadDetailsScreenModel.dart';
 import 'package:liveasy/models/truckModel.dart';
+import 'package:liveasy/screens/myLoadPages/bookLoadScreen.dart';
 import 'package:liveasy/widgets/alertDialog/bookLoadAlertDialogBox.dart';
 import 'package:liveasy/widgets/alertDialog/verifyAccountNotifyAlertDialog.dart';
 import 'package:get/get.dart';
