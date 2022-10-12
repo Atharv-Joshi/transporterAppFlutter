33066a0fdd44be70dc0d3ca255a100c3401e1579
nscspc <83803981+nscspc@users.noreply.github.com>
2022-10-12 19:21:10 +0530

    Update imageDisplayUsingApi.dart

--- lib/screens/TransporterOrders/imageDisplayUsingApi.dart
+++ lib/screens/TransporterOrders/imageDisplayUsingApi.dart
@@ -34,88 +34,88 @@ class _imageDisplayUsingApiState extends State<imageDisplayUsingApi> {
     });
   }
 
+  // ReceivePort _port = ReceivePort();
+
+  // @override
+  // void initState() {
+  //   super.initState();
+
+  //   IsolateNameServer.registerPortWithName(
+  //       _port.sendPort, 'downloader_send_port');
+  //   _port.listen((dynamic data) {
+  //     String id = data[0];
+  //     DownloadTaskStatus status = data[1];
+  //     int progress = data[2];
+  //     setState(() {});
+  //   });
+
+  //   FlutterDownloader.registerCallback(downloadCallback);
+  // }
+
+  // @override
+  // void dispose() {
+  //   IsolateNameServer.removePortNameMapping('downloader_send_port');
+  //   super.dispose();
+  // }
+
+  // static void downloadCallback(
+  //     String id, DownloadTaskStatus status, int progress) {
+  //   final SendPort? send =
+  //       IsolateNameServer.lookupPortByName('downloader_send_port');
+  //   send!.send([id, status, progress]);
+  // }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Align(
-                alignment: Alignment.topRight,
-                child: Container(
-                  margin: EdgeInsets.fromLTRB(space_22, 0, 0, space_1),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(radius_10),
-                    child: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          // clearImage(widget.imageName);
-                          Navigator.pop(context);
-                        });
-                      },
-                      child: Container(
-                        decoration: BoxDecoration(shape: BoxShape.rectangle),
-                        height: space_10,
-                        width: space_10,
-                        child: Center(
+            Container(
+              height: size_15 + 30,
+              color: whiteBackgroundColor,
+              child: Row(
+                children: [
+                  // Flexible(
+                  //   flex: 3,
+                  // child:
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: GestureDetector(
+                          onTap: () {
+                            Get.back();
+                          },
                           child: Icon(
-                            Icons.clear,
+                            Icons.arrow_back_ios,
                             color: darkBlueColor,
-                            size: size_15,
                           ),
                         ),
                       ),
-                    ),
+                      SizedBox(
+                        width: space_1,
+                      ),
+                      Text(
+                        "Download Image".tr,
+                        style: TextStyle(
+                          fontSize: size_10 - 1,
+                          fontWeight: boldWeight,
+                          color: darkBlueColor,
+                          // letterSpacing: -0.408
+                        ),
+                      ),
+                    ],
                   ),
-                ),
+                  // ),
+                ],
               ),
             ),
-
-            // Flexible(
-            // child:
-            Padding(
-                padding: const EdgeInsets.only(left: 15, right: 15, bottom: 8),
-                // Center(
-                //   child: Container(
-                //       margin: EdgeInsets.only(
-                //           bottom: 5, left: 10, right: 10),
-                //       padding:
-                //           EdgeInsets.only(top: space_4),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(5),
-                  child: InkWell(
-                    child: Container(
-                      color: Color.fromARGB(255, 20, 182, 88),
-                      height: space_9,
-                      // width: space_30,
-                      child: Center(
-                        child: progressBar
-                            ? CircularProgressIndicator(
-                                color: white,
-                              )
-                            : Text(
-                                "Download Image".tr,
-                                style: TextStyle(
-                                    color: white,
-                                    fontSize: size_8,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                        // ),
-                      ),
-                    ),
-                    onTapUp: (value) {
-                      setState(() {
-                        progressBar = true;
-                        downloading = true;
-                      });
-                    },
-                    onTap: () {
-                      _saveNetworkImage(widget.docLink.toString());
-                    },
-                  ),
-                )),
+            Divider(
+              height: size_3,
+              color: darkGreyColor,
+            ),
             downloading
                 ? Padding(
                     padding: const EdgeInsets.only(bottom: 5),
@@ -128,7 +128,6 @@ class _imageDisplayUsingApiState extends State<imageDisplayUsingApi> {
                     child: Text("( Downloaded )".tr),
                   )
                 : Container(),
-
             Expanded(
               child: Center(
                 child: Stack(
@@ -152,28 +151,167 @@ class _imageDisplayUsingApiState extends State<imageDisplayUsingApi> {
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: activeButtonColor,
-                  ),
-                  child: Container(
-                    height: space_8,
-                    child: Center(
-                      child: Text(
-                        "Back".tr,
-                        style: TextStyle(
-                            color: white,
-                            fontSize: size_8,
-                            fontWeight: mediumBoldWeight),
+            Row(children: [
+              // Flexible(
+              //     child: ElevatedButton(
+              //   child: Text("Edit"),
+              //   onPressed: () async {
+              //     await _cropImage(
+              //         imageFile: providerData.LrPhotoFile);
+              //   },
+              // )),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 15, right: 7.5, bottom: 10, top: 10),
+                  child: MaterialButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
                       ),
-                    ),
-                  ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-            ),
+                      color: Color(0xFFE75347),
+                      child: Container(
+                        height: space_10,
+                        child: Center(
+                          child: Text(
+                            "Cancel".tr,
+                            style: TextStyle(
+                                color: white,
+                                fontSize: size_9,
+                                fontWeight: mediumBoldWeight),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {
+                        Get.back();
+                      }),
+                ),
+              ),
+              Flexible(
+                child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 7.5, right: 15, bottom: 10, top: 10),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(12),
+                      child: InkWell(
+                        child: Container(
+                          color: Color(0xFF09B778),
+                          height: space_10,
+                          child: Center(
+                            child: progressBar
+                                ? CircularProgressIndicator(
+                                    color: white,
+                                  )
+                                : Text(
+                                    "Download".tr,
+                                    style: TextStyle(
+                                        color: white,
+                                        fontSize: size_8,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                            // ),
+                          ),
+                        ),
+                        onTapUp: (value) {
+                          setState(() {
+                            progressBar = true;
+                            downloading = true;
+                          });
+                        },
+                        onTap: () async {
+                          _saveNetworkImage(widget.docLink.toString());
+
+                          // final status = await Permission.storage.request();
+                          // if (status.isGranted) {
+                          //   final externalDir =
+                          //       await path.getExternalStorageDirectory();
+
+                          // Directory? directory;
+                          // // try {
+                          // if (Platform.isAndroid) {
+                          //   if (status.isGranted) {
+                          //     directory =
+                          //         await path.getExternalStorageDirectory();
+                          // String newPath = "";
+                          // //     print(directory);
+                          // List<String> paths = externalDir!.path.split("/");
+                          // for (int x = 1; x < paths.length; x++) {
+                          //   String folder = paths[x];
+                          //   if (folder != "Android") {
+                          //     setState(() {
+                          //       newPath += "/" + folder;
+                          //     });
+                          //   } else {
+                          //     break;
+                          //   }
+                          //     }
+                          //     print(directory);
+                          //     newPath = newPath + "/LiveasyApp";
+                          //     directory = Directory(newPath);
+                          //     print(directory);
+                          //   } else {
+                          //     // return false;
+                          //   }
+                          // } else {
+                          //   print(directory);
+                          //   final permis =
+                          //       await Permission.photos.request();
+                          //   if (permis.isGranted) {
+                          //     print(directory);
+                          //     directory =
+                          //         await path.getTemporaryDirectory();
+                          //     print(directory);
+                          //   } else {
+                          //     // return false;
+                          //   }
+                          // }
+
+                          // if (!await directory!.exists()) {
+                          //   print(directory);
+                          //   await directory.create(recursive: true);
+                          //   print(directory);
+                          // }
+                          // if (await directory!.exists()) {
+                          //   print(directory);
+                          //   File saveFile = File(directory.path +
+                          //       "/$widget.docLink.toString()");
+
+                          // if (newPath.isNotEmpty)
+                          // final taskId = await FlutterDownloader.enqueue(
+                          //   url: widget.docLink.toString(),
+                          //   // headers: {}, // optional: header send with url (auth token etc)
+                          //   savedDir: externalDir.path,
+                          //   // externalDir!.path,
+                          //   // directory.toString(),
+                          //   // "Internal storage/DCIM/",
+                          //   // externalDir!.path,
+                          //   showNotification:
+                          //       true, // show download progress in status bar (for Android)
+                          //   openFileFromNotification: true,
+                          // );
+
+                          // await FlutterDownloader.registerCallback(
+                          //     downloadCallback); // callback is a top-level or static function
+
+                          // }
+                          // }
+                        },
+                      ),
+                    )),
+                // downloading
+                //     ? Padding(
+                //         padding: const EdgeInsets.only(bottom: 5),
+                //         child: Text("( Downloading.... )".tr),
+                //       )
+                //     : Container(),
+                // downloaded
+                //     ? Padding(
+                //         padding: const EdgeInsets.only(bottom: 5.0),
+                //         child: Text("( Downloaded )".tr),
+                //       )
+                //     : Container(),,
+                // )),
+              ),
+            ]),
           ],
         ),
       ),
