e2d80165c848a710950930e557c92fee5967ff00
ChinmayJoshi1412 <chinmayjoshi1412@gmail.com>
2023-02-05 23:50:46 +0530

    Add Truck fix

---
+++ android/app/build/generated/source/buildConfig/release/com/liveasy/liveasy/BuildConfig.java
@@ -0,0 +1,68 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.liveasy.liveasy;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.liveasy.liveasy";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 100;
+  public static final String VERSION_NAME = "1.0.0";
+  // Field from default config.
+  public static final String biddingApiUrl = "http://bidding.dev.truckseasy.com:8080/bid";
+  // Field from default config.
+  public static final String bookingApiUrl = "http://booking.dev.truckseasy.com:8080/booking";
+  // Field from default config.
+  public static final String buyGPSApiUrl = "http://buygps.dev.truckseasy.com:9090/gps";
+  // Field from default config.
+  public static final String documentApiUrl = "http://document.dev.truckseasy.com:9090/document";
+  // Field from default config.
+  public static final String driverApiUrl = "http://traccar.dev1.truckseasy.com:8082";
+  // Field from default config.
+  public static final String gpsApiUrl = "http://gps.dev.truckseasy.com:3000/locationbyimei";
+  // Field from default config.
+  public static final String idfyAccountId = "ed728f377d5d/1e7581e4-2383-4d11-b6a4-20a72b66c17a";
+  // Field from default config.
+  public static final String idfyApiKey = "ddeb2316-c816-4a9b-9e1d-56ae0b06aa44";
+  // Field from default config.
+  public static final String idfyKey_id = "2753c121-d1d3-4402-8f4f-b896c9a024d5";
+  // Field from default config.
+  public static final String idfyOu_id = "ed728f377d5d";
+  // Field from default config.
+  public static final String idfySecret_Key = "MzM0NGYxYjFiNmMyNDM2OThhMDBhNjMxYWI3MThjOTI=";
+  // Field from default config.
+  public static final String loadApiUrl = "http://load.dev.truckseasy.com:8080/load";
+  // Field from default config.
+  public static final String mapKey = "AIzaSyAhUyHTmaRlAEAD75J1AkMuLH5-zmVmn98";
+  // Field from default config.
+  public static final String mapKeyNew = "AIzaSyDNgqelUhqMZpGcDykhkKDeJroiEsRY4wQ";
+  // Field from default config.
+  public static final String mapKeyOld = "AIzaSyCPU872xIfRhjvtdLViTDbj0EnIBuxlcSs";
+  // Field from default config.
+  public static final String oneSignalApiKey = "NjI5N2JiNGYtNWZhNC00NDE0LThlMGItNTk5ODQ2ZDIyZDUx";
+  // Field from default config.
+  public static final String oneSignalAppId = "b1948857-b2d1-4946-b4d1-86f911c30389";
+  // Field from default config.
+  public static final String rapidKey = "7b196c1d3amsh419883f34f424f1p138fcajsn819528e5e9fa";
+  // Field from default config.
+  public static final String routeDataApiUrl = "http://load.dev.truckseasy.com:8080/routedata";
+  // Field from default config.
+  public static final String routeHistoryApiUrl = "http://gps.dev.truckseasy.com:3000/routehistory";
+  // Field from default config.
+  public static final String shareUrl = "https://liveasywa.page.link";
+  // Field from default config.
+  public static final String shipperApiUrl = "http://shipper.dev.truckseasy.com:9090/shipper";
+  // Field from default config.
+  public static final String traccarApi = "http://traccar.dev1.truckseasy.com:8082/api";
+  // Field from default config.
+  public static final String traccarPass = "admin";
+  // Field from default config.
+  public static final String traccarUser = "admin";
+  // Field from default config.
+  public static final String transporterApiUrl = "http://transporter.dev.truckseasy.com:9090/transporter";
+  // Field from default config.
+  public static final String truckApiUrl = "http://truck.dev.truckseasy.com:9090/truck";
+  // Field from default config.
+  public static final String webHookUrl = "https://webhook.site/3433a9c3-4c93-4427-8ce6-b310300d2cd7";
+}
