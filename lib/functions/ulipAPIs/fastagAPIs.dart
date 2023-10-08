57ff8dcd01c60eb7ed9258c263ac29749d0c7a13
Vedant Fuley <vedantfuley@gmail.com>
2023-10-08 22:47:31 +0530

    Fastag APIs function.

---
+++ lib/functions/ulipAPIs/fastagAPIs.dart
@@ -0,0 +1,34 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../../models/fastagModel.dart';
+import 'package:flutter_config/flutter_config.dart';
+
+Future<List<TollPlazaData>> fetchTollPlazaData(String vehicleNumber) async {
+  try {
+    final String fastagApiUrl = FlutterConfig.get('fastagApiUrl').toString();
+    final response = await http.post(
+      Uri.parse(fastagApiUrl),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: jsonEncode({'vehiclenumber': vehicleNumber}),
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonResponse = json.decode(response.body);
+
+      final List<dynamic> txnList = jsonResponse['response'][0]['response']
+          ['vehicle']['vehltxnList']['txn'];
+
+      final List<TollPlazaData> tollPlazaDataList =
+          txnList.map((data) => TollPlazaData.fromJson(data)).toList();
+
+      return tollPlazaDataList;
+    } else {
+      throw Exception('Failed to load toll plaza data');
+    }
+  } catch (e) {
+    return [];
+  }
+}
