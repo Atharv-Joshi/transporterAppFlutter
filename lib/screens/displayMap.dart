12c526f25132f167f20dd84a5fe85231c0558857
Vedant Fuley <vedantfuley@gmail.com>
2023-11-21 19:58:31 +0530

    flutter_polyline_points has been removed due to version incompatibility.

--- lib/screens/displayMap.dart
+++ lib/screens/displayMap.dart
@@ -6,7 +6,6 @@ import 'package:geolocator/geolocator.dart';
 import 'package:liveasy/constants/color.dart';
 import 'package:liveasy/constants/spaces.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:liveasy/functions/mapUtils/getLoactionUsingImei.dart';
 import 'package:liveasy/widgets/Header.dart';
